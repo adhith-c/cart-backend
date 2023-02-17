23e75ad3448edec86c2455d6198322dce7192308
adhith-c <adhithadhi17@gmail.com>
2023-02-17 22:29:31 +0530

    products added

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const cartController = require("../controllers/cartController");
 const router = express.Router();
+router.get("/getProducts", cartController.getProducts);
 router.get("/", cartController.getCart);
 router.post("/addtocart", cartController.addToCart);
 router.post("/incQuantity", cartController.incrementQuantity);
